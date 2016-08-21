98f6b69b5a535839099e14ea8b39da9de4062137
felixacdc <pfmata01@gmail.com>
2016-08-21 14:35:14 -0600

    [add] 10. Repaso de creación y uso de componentes en Vue.js (finish)

--- Classes/js/main.js
+++ Classes/js/main.js
@@ -13,9 +13,40 @@ Vue.component('select-category', {
     props: ['categories', 'id']
 });
 
+Vue.filter('category', function(id) {
+    var category = findById(this.categories, id);
+
+    return category != null ? category.name : 'Categoria no encontrada';
+});
+
+Vue.component('note-row', {
+    template: '#note_row_tpl',
+    props: ['note', 'categories'],
+    data: function() {
+        return {
+            editing: false
+        }
+    },
+    methods: {
+        remove: function() {
+            this.$parent.notes.$remove(this.note);
+        },
+        edit: function() {
+            this.editing = true
+        },
+        update: function() {
+            this.editing = false;
+        }
+    }
+})
+
 var vm = new Vue({
     el: 'body',
     data: {
+        new_note: {
+            note: '',
+            category_id: ''
+        },
         notes: [
             {
                 note: 'Laravel 5.1 es LTS',
@@ -54,21 +85,11 @@ var vm = new Vue({
         ]
     },
     methods: {
-        deleteNote: function(note) {
-            this.notes.$remove(note);
-        },
-        editNote: function(note) {
-            Vue.set(note, 'editing', true);
-        },
-        updateNote: function(note) {
-            note.editing = false;
+        createNote: function() {
+            this.notes.push(this.new_note);
+            this.new_note = {note: '', category_id: ''};
         }
     },
     filters: {
-        category: function(id) {
-            var category = findById(this.categories, id);
-
-            return category != null ? category.name : 'Categoria no encontrada';
-        }
     }
 });
